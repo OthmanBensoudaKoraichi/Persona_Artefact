0ab2792f44e7789827fe81f3820691ae125bbd17
OthmanBensouda <othmanb@stanford.edu>
2024-05-07 08:57:38 -0700

    added images

--- Accueil.py
+++ Accueil.py
@@ -1,21 +1,20 @@
 from src.utils import style,config
 import streamlit as st
 from st_clickable_images import clickable_images
+from streamlit_extras.switch_page_button import switch_page
 
 def main():
     # Set style
 
-    style.set_bg_image(config.doodle)
-
-
+    style.set_bg_image(config.background)
 
     clicked = clickable_images(
         [
-            "https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=700",
-            "https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=700",
-            "https://images.unsplash.com/photo-1582550945154-66ea8fff25e1?w=700",
-            "https://images.unsplash.com/photo-1591797442444-039f23ddcc14?w=700",
-            "https://images.unsplash.com/photo-1518727818782-ed5341dbd476?w=700",
+            config.first_persona,
+            config.second_persona,
+            config.third_persona,
+            config.fourth_persona,
+            config.fifth_persona,
         ],
         titles=[f"Image #{str(i)}" for i in range(5)],
         div_style={"display": "flex", "justify-content": "center", "flex-wrap": "wrap"},
@@ -23,6 +22,13 @@ def main():
     )
 
     st.markdown(f"Image #{clicked} clicked" if clicked > -1 else "No image clicked")
+
+    if clicked == 0:
+        switch_page("détails des personae")
+
+    if clicked == 1:
+        switch_page("générateur de créas")
+
 
     style.create_interface()
 
--- src/utils/config.py
+++ src/utils/config.py
@@ -1,2 +1,12 @@
-doodle = "https://github.com/OthmanBensoudaKoraichi/Persona_Artefact/blob/master/imgs/BOA.jpg?raw=true"
+background = "https://github.com/OthmanBensoudaKoraichi/Persona_Artefact/blob/master/imgs/BOA.jpg?raw=true"
 data_boa = "/Users/othmanbensouda/PycharmProjects/Persona_Artefact/sample_data_boa.xlsx"
+first_persona = "https://github.com/OthmanBensoudaKoraichi/Persona_Artefact/blob/master/imgs/Hanae.png?raw=true"
+second_persona = "https://github.com/OthmanBensoudaKoraichi/Persona_Artefact/blob/master/imgs/Karim.png?raw=true"
+third_persona = "https://github.com/OthmanBensoudaKoraichi/Persona_Artefact/blob/master/imgs/Lina.png?raw=true"
+fourth_persona = "https://github.com/OthmanBensoudaKoraichi/Persona_Artefact/blob/master/imgs/Sophia.png?raw=true"
+fifth_persona = "https://github.com/OthmanBensoudaKoraichi/Persona_Artefact/blob/master/imgs/Amine.png?raw=true"
+generator = "https://github.com/OthmanBensoudaKoraichi/Persona_Artefact/blob/master/imgs/generator.png?raw=true"
+logo_boa = "https://github.com/OthmanBensoudaKoraichi/Persona_Artefact/blob/master/imgs/logo_boa.png?raw=true"
+explorator = "https://github.com/OthmanBensoudaKoraichi/Persona_Artefact/blob/master/imgs/explorator.png?raw=true"
+
+

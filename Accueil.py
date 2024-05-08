382d7f309cebcaf8a18c8211321cb5bb6f3db2bd
OthmanBensouda <othmanb@stanford.edu>
2024-05-07 21:34:45 -0700

    changed explorator and generator images for landing pages

--- Accueil.py
+++ Accueil.py
@@ -5,7 +5,7 @@ from streamlit_extras.switch_page_button import switch_page
 
 def main():
     # Set style
-    st.set_page_config(layout="wide",initial_sidebar_state="collapsed")
+    st.set_page_config(layout="wide",initial_sidebar_state="collapsed",page_icon = config.favicon)
 
     style.set_bg_image(config.background)
     style.display_logo(config.logo_boa)
--- src/utils/config.py
+++ src/utils/config.py
@@ -2,8 +2,8 @@ background = "https://github.com/OthmanBensoudaKoraichi/Persona_Artefact/blob/ma
 data_boa = "/Users/othmanbensouda/PycharmProjects/Persona_Artefact/sample_data_boa.xlsx"
 generator = "https://github.com/OthmanBensoudaKoraichi/Persona_Artefact/blob/master/imgs/generator.jpg?raw=true"
 logo_boa = "https://github.com/OthmanBensoudaKoraichi/Persona_Artefact/blob/master/imgs/logo_boa.png?raw=true"
-explorator = "https://github.com/OthmanBensoudaKoraichi/Persona_Artefact/blob/master/imgs/explorator.png?raw=true"
-favicon =
+explorator = "https://github.com/OthmanBensoudaKoraichi/Persona_Artefact/blob/master/imgs/explorator.jpg?raw=true"
+favicon = "https://github.com/OthmanBensoudaKoraichi/Persona_Artefact/blob/master/imgs/boa_favicon.png?raw=true"
 
 # Persona presentations
 persona_images = {

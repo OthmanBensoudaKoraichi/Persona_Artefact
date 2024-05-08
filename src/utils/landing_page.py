364d1d9e678e749901206ca3c3c849b9cfe855fa
OthmanBensouda <othmanb@stanford.edu>
2024-05-07 22:29:21 -0700

    changed landing page images

--- src/utils/landing_page.py
+++ src/utils/landing_page.py
@@ -19,13 +19,14 @@ def display_landing_page(explorator_path, generator_path):
     col1, col2 = st.columns(2)
 
     with col1:
+        st.subheader("Générateur de Créas")
+        st.markdown(f'<div class="responsive-image"><img src="{generator_path}"></div>', unsafe_allow_html=True)
+        st.write("Lancez des campagnes percutantes avec un outil intuitif de création de campagnes. Le générateur de campagne vous guide selon votre besoin afin de générer le bon format avec des messages pertinents.")
+
+    with col2:
         st.subheader("Détails des segments")
         st.markdown(f'<div class="responsive-image"><img src="{explorator_path}"></div>', unsafe_allow_html=True)
         st.write("Découvrez le cœur de votre audience avec le module avancé de segmentation. Transformez les insights en action et personnalisez votre approche pour atteindre votre public cible de manière plus efficace que jamais.")
 
-    with col2:
-        st.subheader("Générateur de Créas")
-        st.markdown(f'<div class="responsive-image"><img src="{generator_path}"></div>', unsafe_allow_html=True)
-        st.write("Lancez des campagnes percutantes avec un outil intuitif de création de campagnes. Le générateur de campagne vous guide selon votre besoin afin de générer le bon format avec des messages pertinents.")
 
     return
